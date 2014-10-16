e8d1164900cf53fdeceb0b26c2af229d44206284
jimcurry <jim.j.curry@gmail.com>
2014-10-16 17:09:50 -0400

    Added sort to report info retrieval.

--- src/main/java/com/lpasystems/premieraco/dao/ReportInfoDAO.java
+++ src/main/java/com/lpasystems/premieraco/dao/ReportInfoDAO.java
@@ -24,7 +24,8 @@ public interface ReportInfoDAO {
 	@SqlQuery("SELECT rpt_cd " +
 		      ", rpt_spfcn_id " +
 		 "FROM rpt_dim " +
-		"where pce_cst_nm = #pce_cst_nm "
+		"where pce_cst_nm = #pce_cst_nm " +
+		"order by rpt_cd "
 		 )
 	// @formatter:on
 	List<ReportInfo> getReportInfoList(@Bind("pce_cst_nm") String pce_cst_nm);
